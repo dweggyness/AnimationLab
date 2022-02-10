f5835ae6904abed950c5eca7d3e6aa3eaabf4e3c
Jun Ming Ooi <ojmjunming@gmail.com>
2022-02-10 14:19:32 +0400

    anim lab 22 mins commit

---
+++ Assets/Scripts/CubeController.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeController : MonoBehaviour
+{
+    public Transform start;
+    public Transform end;
+
+    public float speed = 0.3f;
+
+    [Range(0, 1)]
+    public float t = 0.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.position = start.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        t = Mathf.MoveTowards(t, 1.0f, speed * Time.deltaTime);
+
+        transform.position = Vector3.Lerp(start.position, end.position, t);
+        transform.rotation = Quaternion.Slerp(start.rotation, end.rotation, t);
+    }
+}
